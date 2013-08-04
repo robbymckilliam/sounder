186326dc7730c2ff3b3e488b58a4e3f6592e3ac2
robbymckilliam <robbymckilliam@gmail.com>
2013-08-04 23:13:46 +0930

    added second derivative of sinc

--- src/sounder/Util.scala
+++ src/sounder/Util.scala
@@ -1,11 +1,13 @@
 package sounder
 
 import scala.math.sin
+import scala.math.cos
 import scala.math.min
 import scala.math.max
 import scala.math.floor
 import scala.math.ceil
 import scala.math.Pi
+import scala.math.pow
 
 /**
  * Static object containing commonly used functions. 
@@ -20,6 +22,22 @@ object Util {
       return  1.0 - t2*( 1.0/6 - 1.0/120*t2 )
     }
     else return sin(Pi*t)/(Pi*t)
+  }
+  
+  /** First derivative of the sinc function */
+  def dsinc(t : Double) : Double = { 
+    if(t.abs < 5e-3 ) {  //use a 4th order expansion if t is small
+      return  -Pi*Pi*t/3.0 + pow(Pi,4)*t*t*t/30.0
+    }
+    else return (Pi*t*cos(Pi*t) - sin(Pi*t)) / (Pi*t*t) 
+  }
+  
+    /** Second derivative of the sinc function */
+  def d2sinc(t : Double) : Double = { 
+    if(t.abs < 5e-3 ) {  //use a 4th order expansion if t is small
+      return  Pi*Pi*( -1.0/3 + pow(Pi,4)*t*t/10 - pow(Pi,6)*pow(t,4)/168)
+    }
+    else return ((2 - Pi*Pi*t*t)*sin(Pi*t) -2*Pi*t*cos(Pi*t) )/Pi/t/t/t;
   }
 
 }
--- test/sounder/UtilTest.scala
+++ test/sounder/UtilTest.scala
@@ -7,6 +7,7 @@ package sounder
 import org.junit.Test
 import org.junit.Assert._
 import sounder.Util._
+import scala.math.Pi
 
 class UtilTest {
   
@@ -22,6 +23,33 @@ class UtilTest {
     assertEquals(sinc(-0.5),0.6366197724,tol)
     assertEquals(sinc(-0.5),0.6366197724,tol)
     assertEquals(sinc(-10.5),0.03031522726,tol)
+  }
+  
+  @Test
+  def dSincTest() {
+    println("Testing derivative of sinc")
+    val tol = 1e-8
+    //output from Mathematica
+    assertEquals(dsinc(0),0.0,tol)
+    assertEquals(dsinc(0.1),-0.3257512679,tol)
+    assertEquals(dsinc(-0.2),0.6323614471,tol)
+    assertEquals(dsinc(0.2),-0.6323614471,tol)
+    assertEquals(dsinc(-33.0/10.0),0.1544695340,tol)
+    assertEquals(dsinc(33.0/10.0),-0.1544695340,tol)
+  }
+  
+  @Test
+  def d2SincTest() {
+    println("Testing second derivative of sinc")
+    val tol = 1e-8
+    //output from Mathematica
+    assertEquals(d2sinc(0),-Pi*Pi/3,tol)
+    assertEquals(d2sinc(1e-6),-3.289868134,tol)
+    assertEquals(d2sinc(-1e-6),-3.289868134,tol)
+    assertEquals(d2sinc(0.1),-3.193029836,tol)
+    assertEquals(d2sinc(-0.1),-3.193029836,tol)
+    assertEquals(d2sinc(11.0/10),2.306741312,tol)
+    assertEquals(d2sinc(-11.0/10),2.306741312,tol)
   }
   
 }
